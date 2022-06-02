20626d958d2bdd0bd0d39675e8c3371f1f1b530a
Ayobami Tewogbade <roybylon@gmail.com>
2022-06-02 21:56:56 +0000

    add a program that prints a string using puts function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str2[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%c", str2);
+return (0);
+}
