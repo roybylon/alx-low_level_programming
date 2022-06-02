5f6874d9fb0b2f1068b28f70e15854c411a802e6
Ayobami Tewogbade <roybylon@gmail.com>
2022-06-02 22:12:56 +0000

    add a program that prints a string using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char str2[] = "with proper grammar, but the outcome is a piece of art, ";
+char str2[] = "with proper grammar, but the outcome is a piece of art, \n";
 printf("%s", str2);
 return (0);
 }
